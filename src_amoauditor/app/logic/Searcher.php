7cd53fdeb96628464656f86dcd61cc6252676296
Prigaro Nikita <nprigaro@team.amocrm.com>
2022-08-11 16:52:34 +0300

    string shortener not working

--- src_amoauditor/app/logic/Searcher.php
+++ src_amoauditor/app/logic/Searcher.php
@@ -16,6 +16,21 @@ class Searcher
     public function search(string $searchParam, string $jsFilePath): array {
         $searchResultInJsFile = [];
         exec('grep -n '.$searchParam." ".$jsFilePath, $searchResultInJsFile);
+
+        //обрезка обусфицированного кода
+        for ($i = 0; $i < count($searchResultInJsFile); $i++) {
+            $result = explode($searchResultInJsFile[$i],':', 2);
+            $position = strpos($result[1], $searchParam);
+
+            $result[1] = substr(
+                $result[1],
+                $position > 50 ? $position - 50 : 0,
+                150
+            );
+
+            $searchResultInJsFile[$i] = implode(':          ', $result);
+        }
+
         return $searchResultInJsFile;
     }
 
